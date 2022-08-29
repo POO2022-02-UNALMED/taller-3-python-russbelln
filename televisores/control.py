c52d4c692d3cf802b37c0f3a7a4a4eae0d35e5b5
russbelln <rnorena@unal.edu.co>
2022-08-29 13:22:33 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -21,8 +21,8 @@ class Control:
     def enlazar(self, tv):
         self.tv = tv
         tv.setControl(self)
-    def getTV(self):
+    def getTv(self):
         return self.tv
-    def setTV(self, tv):
+    def setTv(self, tv):
         self.tv = tv
         tv.setControl(self)
